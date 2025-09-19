d39254c51134aa21ee7ade4057b0a1bb1ece5e33
LuanY77 <2307984361@qq.com>
2025-09-19 16:29:23 +0800

    init project

---
+++ src/main/java/com/lunay/deepresearch/LiteflowAiDeepResearchApplication.java
@@ -0,0 +1,13 @@
+package com.lunay.deepresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteflowAiDeepResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteflowAiDeepResearchApplication.class, args);
+    }
+
+}
